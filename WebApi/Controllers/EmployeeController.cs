7682c20770296f34f7b764f598a74da9de840064
Sujata Jagtap <sujata_jagtap@persistent.com>
2022-11-04 11:35:21 +0530

    initial commit

---
+++ WebApi/Controllers/EmployeeController.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using System.Web.Mvc.Razor;
+using System.Web.UI.WebControls;
+using WebApi.Models;
+
+namespace WebApi.Controllers
+{
+    public class EmployeeController : ApiController
+    {
+        Employee2Entities db=new Employee2Entities();
+
+        [HttpGet]
+        public IHttpActionResult GetEmployee()
+        {
+            List<Employee2> list = db.Employee2.ToList();
+            return Ok(list);
+            
+               
+        }
+        [HttpPost]
+        public IHttpActionResult EmpInsert(Employee2 e)
+        {
+            db.Employee2.Add(e);
+            db.SaveChanges();
+            return Ok();
+
+        }
+  
+
+
+
+
+
+
+
+
+
+
+        /*public string[] Employees = { "Sujata", "Vaishnavi" };
+        [HttpGet]
+        public string[] GetEmployee()
+        {
+            return Employees;
+        }
+        [HttpGet]
+        public string GetEmployeeByIndex(int id)
+
+        {
+            return Employees[id];
+        }*/
+       
+    }
+}
+
+
+
+
+
---
+++ WebApi/Controllers/MVCController.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Web;
+using System.Web.Mvc;
+using WebApi.Models;
+
+namespace WebApi.Controllers
+{
+    public class MVCController : Controller
+    {
+        // GET: MVC
+        HttpClient Client=new HttpClient();
+
+        public ActionResult Index()
+        {
+            List<Employee2> emp_list = new List<Employee2>();
+            Client.BaseAddress = new Uri("https://localhost:44392/api/Employee");
+            var response = Client.GetAsync("Employee");
+            response.Wait();
+            var test=response.Result;
+            if (test.IsSuccessStatusCode)
+            {
+                var display = test.Content.ReadAsAsync< List < Employee2 >>();
+                display.Wait();
+               
+                emp_list = display.Result; 
+
+            }
+
+            return View(emp_list);
+        }
+        public ActionResult Create()
+        {
+            return View();
+        }
+        [HttpPost]
+        public ActionResult Create(Employee2 emp)
+        {
+            Client.BaseAddress = new Uri("https://localhost:44392/api/Employee");
+            var response = Client.PostAsJsonAsync<Employee2>("Employee",emp);
+            response.Wait();
+            var test = response.Result;
+            if (test.IsSuccessStatusCode)
+            {
+                return RedirectToAction("Index");
+
+            }
+
+            return View("Create");
+
+        }
+    }
+}
